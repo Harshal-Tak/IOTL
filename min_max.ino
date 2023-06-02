f24a6b5eb5cc8afe35fb6d139473d1c6e4ea20cf
Harshal Tak <127540186+Harshal-Tak@users.noreply.github.com>
2023-06-02 12:01:11 +0530

    Update min_max.ino

--- min_max.ino
+++ min_max.ino
@@ -33,3 +33,45 @@ void loop()
   Serial.print("\n");
 }
 
+/*#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN A1
+
+float min_t, max_t;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Humidity(%)\tTemperature(C)\tTemperature(F)\tMinTemp\tMaxTemp");
+  min_t = 100.0;
+  max_t = -100.0;
+}
+
+void loop() {
+  int chk = DHT.read11(DHT11_PIN);
+  float temperature_C = DHT.temperature;
+  float temperature_F = (temperature_C * 1.8) + 32.0;
+
+  if (temperature_C < min_t) {
+    min_t = temperature_C;
+  }
+
+  if (temperature_C > max_t) {
+    max_t = temperature_C;
+  }
+
+  Serial.print(DHT.humidity, 1);
+  Serial.print("\t");
+  Serial.print(temperature_C, 1);
+  Serial.print("\t");
+  Serial.print(temperature_F, 1);
+  Serial.print("\t");
+  Serial.print(min_t, 1);
+  Serial.print("\t");
+  Serial.print(max_t, 1);
+  Serial.println();
+  
+  delay(1000);
+}
+*/
