235a1ec53f53d999402c24e3e4303ebd4325f367
Harshal Tak <127540186+Harshal-Tak@users.noreply.github.com>
2023-06-02 12:02:02 +0530

    Update 8.ino

--- 8.ino
+++ 8.ino
@@ -11,3 +11,18 @@ void loop() {
   Serial.println(int(squaredInput));
 
 }
+
+/*void setup() {
+  Serial.begin(9600);
+  Serial.println("Input any number: ");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    int input = Serial.parseInt();
+    int squaredInput = sq(input);
+    Serial.println(squaredInput);
+  }
+}
+*/
